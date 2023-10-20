121115ad99b34fcbea8716a99f0b8dad6c20542e
shiningstar12345 <147730405+shiningstar12345@users.noreply.github.com>
2023-10-19 22:04:07 -0500

    Update prueba.py
    Cambio en prueba py

---
+++ prueba.py
@@ -0,0 +1,27 @@
+Circulo(200,200,150,relleno='oro')
+Arco(200,200,300,300,0,108,relleno='cian')
+Arco(200,200,300,300,108,90,relleno='rosado')
+Arco(200,200,300,300,198,54,relleno='naranja')
+Arco(200,200,300,300,252,22,relleno='rojo')
+Arco(200,200,300,300,274,18,relleno='ladrillo')
+Arco(200,200,300,300,292,4,relleno='lila')
+Arco(200,200,300,300,296,54,relleno='verde')
+
+
+
+
+
+
+
+
+
+
+
+
+#cristianismo=30%:cian
+#islam=25%:rosado
+#hinduismo=15%:naranja
+#budismo=6%:rojo
+#religiones tradicionasles=5%:ladrillo
+#religiones afroamericanas =1%:lila
+#personas no religiosas=15%:verde
